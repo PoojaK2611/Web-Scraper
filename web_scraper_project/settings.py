7bf127abc3bce168575ae96eb37b3e8d644092e5
unknown <pooja.jagtap1391@gmail.com>
2020-03-30 01:01:33 +0530

    changes in HOST settings

--- web_scraper_project/settings.py
+++ web_scraper_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'dbwebscrapper',
         'USER': 'bf527fe52c9f20',
         'PASSWORD': '2ec787a42f0de00',
-        'HOST': 'mysql://bf527fe52c9f20:f72ee79a@us-cdbr-iron-east-01.cleardb.net/heroku_1c25bf446e4b4ee?reconnect=true'
+        'HOST': 'us-cdbr-iron-east-01.cleardb.net/heroku_1c25bf446e4b4ee'
     }
 }
 
